600a495b695eff8ff6fe64008378361ff3f66984
Hanane Mouder <hanane.mouder@docaposte.fr>
2025-12-01 23:51:12 +0100

    init categories services

---
+++ src/services/category.api.ts
@@ -0,0 +1,56 @@
+import type { Category, CategoryInput } from "../types/categories.type";
+
+const API_URL = "http://localhost:4000/admin/categories";
+
+function getToken() {
+  return localStorage.getItem("TOKEN");
+}
+
+export async function getCategories(): Promise<Category[]> {
+  const res = await fetch(API_URL, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+
+  if (!res.ok) throw new Error("Erreur chargement catégories");
+  return res.json();
+}
+
+export async function createCategory(input: CategoryInput): Promise<Category> {
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getToken()}`,
+    },
+    body: JSON.stringify(input),
+  });
+
+  if (!res.ok) throw new Error("Erreur création catégorie");
+  return res.json();
+}
+
+export async function updateCategory(
+  id: string,
+  input: CategoryInput
+): Promise<Category> {
+  const res = await fetch(`${API_URL}/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getToken()}`,
+    },
+    body: JSON.stringify(input),
+  });
+
+  if (!res.ok) throw new Error("Erreur mise à jour catégorie");
+  return res.json();
+}
+
+export async function deleteCategory(id: string): Promise<void> {
+  const res = await fetch(`${API_URL}/${id}`, {
+    method: "DELETE",
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+
+  if (!res.ok) throw new Error("Erreur suppression catégorie");
+}
---
+++ src/types/categories.type.ts
@@ -0,0 +1,11 @@
+export type Category = {
+  id: string;
+  name: string;
+  description?: string;
+  createdAt: string;
+  updatedAt: string;
+};
+export interface CategoryInput {
+  name: string;
+  description: string;
+}
