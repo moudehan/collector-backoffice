71d15afb88d603927cc11eb66ea783d04db5cab9
Hanane Mouder <hanane.mouder@docaposte.fr>
2025-12-04 00:31:13 +0100

    init websockt to frauds

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,18 +1,21 @@
 import { useEffect } from "react";
 import { io } from "socket.io-client";
-import type { Article } from "../types/articles.type";
+import type { FraudAlert } from "../types/fraud.type";
 
-export function useFraudAlerts(onAlert: (alert: Article) => void) {
+export function useFraudAlerts(onAlert: (alert: FraudAlert) => void) {
   useEffect(() => {
     const socket = io("http://localhost:4000", {
       transports: ["websocket"],
+      auth: {
+        token: localStorage.getItem("TOKEN"),
+      },
     });
 
     socket.on("connect", () => {
       console.log("WS connecté !");
     });
 
-    socket.on("new_fraud_alert", (alert) => {
+    socket.on("new_fraud_alert", (alert: FraudAlert) => {
       console.log("⚡ Nouvelle alerte reçue :", alert);
       onAlert(alert);
     });
